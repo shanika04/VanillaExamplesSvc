fdb41f51b480b6aa315b597b99e29ff05edc81bd
shanika04 <73774345+shanika04@users.noreply.github.com>
2020-12-21 18:36:14 +0200

    undo fix

--- src/main/java/xxe/XXESource.java
+++ src/main/java/xxe/XXESource.java
@@ -1,3 +1,4 @@
+
 package xxe;
 
 import java.io.FileInputStream;
@@ -10,10 +11,6 @@ public class XXESource {
 
     private void source(String filename) throws XMLStreamException {
         XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-        // Restriction of XML External Entity Reference (CWE 611)
-        inputFactory.setProperty(XMLInputFactory.ACCESS_EXTERNAL_DTD, false);
-        // Restriction of XML External Entity Reference (CWE 611)
-        inputFactory.setProperty(XMLInputFactory.ACCESS_EXTERNAL_SCHEMA, false);
         new FactorySink().getXml(filename, inputFactory);
     }
 }
