74bf694ddcffb0ba7db4967d60b53e96ad69908d
shanika04 <73774345+shanika04@users.noreply.github.com>
2020-12-21 18:33:18 +0200

    undo fix

--- src/main/java/sqli/SQLTest6.java
+++ src/main/java/sqli/SQLTest6.java
@@ -10,17 +10,7 @@ public class SQLTest6 {
 
     public void sqlTest6(String ip) {
         try {
-            PreparedStatement myPreparedStatement =
-                    getJDBCConnection()
-                            .prepareStatement(
-                                    "INSERT INTO banned_ip(id, ip) VALUE('"
-                                            + UUID.randomUUID().toString()
-                                            + "','"
-                                            + "?"
-                                            + "')");
-            // Setting the query parameters
-            myPreparedStatement.setString(1, ip);
-            myPreparedStatement.execute();
+            getJDBCConnection().createStatement().execute("INSERT INTO banned_ip(id, ip) VALUE('" + UUID.randomUUID().toString() + "','" + ip + "')");
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
