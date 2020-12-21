450e1d6371021b108a864231c845c2060d9109fb
shanika04 <73774345+shanika04@users.noreply.github.com>
2020-12-21 18:29:58 +0200

    undo fix

--- src/main/java/sqli/SQLTest15.java
+++ src/main/java/sqli/SQLTest15.java
@@ -9,27 +9,15 @@ import java.util.UUID;
 public class SQLTest15 {
 
     public void sqlTest15(boolean addExt, String ip) {
-        String ipUnsafe = null;
-        String ipUnsafe = null;
         if (addExt) {
-            ipUnsafe = ip;
-            ip = "?" + ".a";
+            ip = ip + ".a";
         } else {
-            ipUnsafe = ip;
-            ip = "?" + ".b";
+            ip = ip + ".b";
         }
         try {
-            String sql =
-                    "INSERT INTO banned_ip(id, ip) VALUE('"
-                            + UUID.randomUUID().toString()
-                            + "','"
-                            + ip
-                            + "')";
-            PreparedStatement statement = getJDBCConnection().prepareStatement(sql);
-            statement.setString(2, ipUnsafe);
-            // Setting the query parameters
-            statement.setString(1, ipUnsafe);
-            statement.execute();
+            String sql = "INSERT INTO banned_ip(id, ip) VALUE('" + UUID.randomUUID().toString() + "','" + ip + "')";
+            Statement statement = getJDBCConnection().createStatement();
+            statement.execute(sql);
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
