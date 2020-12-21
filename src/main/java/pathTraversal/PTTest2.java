10d770d61c5bdb8d084a6fcd0cbb4a40ee080367
shanika04 <73774345+shanika04@users.noreply.github.com>
2020-12-21 18:27:17 +0200

    undo fix

--- src/main/java/pathTraversal/PTTest2.java
+++ src/main/java/pathTraversal/PTTest2.java
@@ -5,13 +5,9 @@ import java.io.File;
 
 public class PTTest2 {
     private final String BASE_DIRECTORY = "src";
-
     public void Test(String path) {
         try {
             File dictionaryFile = new File(BASE_DIRECTORY + "abc/" + path);
-            if (dictionaryFile.getCanonicalPath().startsWith(new File("srcabc/").getCanonicalPath())) {
-                throw new PathTraversalException("invalid file path entered");
-            }
             dictionaryFile.delete();
 
         } catch (Exception ex) {
@@ -19,4 +15,5 @@ public class PTTest2 {
         }
         System.out.print("sdfsf");
     }
+
 }
